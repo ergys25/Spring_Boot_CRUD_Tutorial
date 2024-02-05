a603e737af7fae65109ed3ba79b97d4e82c7e8fb
Ergys Hila <ergysmania@gmail.com>
2024-02-05 17:15:06 +0200

    Connected the postgres database.

--- src/main/java/com/ergyshila/fullstack_backend/model/User.java
+++ src/main/java/com/ergyshila/fullstack_backend/model/User.java
@@ -2,8 +2,10 @@ package com.ergyshila.fullstack_backend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Users")
 public class User {
     @Id
     @GeneratedValue
