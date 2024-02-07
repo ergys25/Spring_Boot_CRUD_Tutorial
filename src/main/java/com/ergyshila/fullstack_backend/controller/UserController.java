720b417a9644400a5452d2ae5c6d4709c3d2167e
Ergys Hila <ergysmania@gmail.com>
2024-02-07 11:37:07 +0200

    Added endpoints

--- src/main/java/com/ergyshila/fullstack_backend/controller/UserController.java
+++ src/main/java/com/ergyshila/fullstack_backend/controller/UserController.java
@@ -2,12 +2,11 @@ package com.ergyshila.fullstack_backend.controller;
 import com.ergyshila.fullstack_backend.repository.UserRepository;
 import com.ergyshila.fullstack_backend.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
 
